bb60bfd8f790f25f5fd8d3a4fa32f128a3475f25
Hasibul <hasibulislam4500@gmail.com>
2021-06-12 11:15:31 +0600

    Adding main scrapper file

---
+++ source/scraper.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup as BST
+
+# Getting username to make url.
+githubUsername = input('Input GitHub Username: ')
+
+url = 'https://github.com/' + githubUsername
+
+request = requests.get(url)
+
+htmlContent = BST(request.content, 'html.parser')
+
+profileImage = htmlContent.find('img', {'alt' : 'Avatar'})['src']
+
+numberOfRepo = htmlContent.find('span', {'class' : 'Counter'})['title']
+
+print(profileImage)
+print('Number of Repositories: ' + numberOfRepo)
+
